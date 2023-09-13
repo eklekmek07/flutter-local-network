e6008792a60fd0139d3c92793cda196510f27c87
Barış <baris_kilinc@windowslive.com>
2023-09-14 02:50:25 +0300

    init flutter project

---
+++ local_network_demo/README.md
@@ -0,0 +1,3 @@
+# local_network_demo
+
+A new Flutter project.
---
+++ local_network_demo/android/app/src/main/kotlin/com/example/local_network_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.local_network_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
