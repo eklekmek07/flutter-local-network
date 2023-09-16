f8fba74a9521234b132a635c2c11c04318d05a10
Barış <baris_kilinc@windowslive.com>
2023-09-17 01:33:39 +0300

    try to add list but scanner not working

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:dart_ping_ios/dart_ping_ios.dart';
 import 'package:flutter/material.dart';
+import 'package:lan_scanner/lan_scanner.dart';
 import 'dart:async';
 import 'package:network_info_plus/network_info_plus.dart';
 
 void main() {
+  DartPingIOS.register();
+  Future.delayed(const Duration(seconds: 5));
   runApp(const MainApp());
 }
 
@@ -14,17 +18,8 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
-  List<String> discoveredDevices = [];
-  String textData = "";
-
-  Future<void> getIPAddress() async {
-    String? data = await NetworkInfo().getWifiIP();
-    if (data != null) {
-      setState(() {
-        textData = data;
-      });
-    }
-  }
+  List<dynamic> discoveredDevices = [];
+  String localIp = "";
 
   @override
   Widget build(BuildContext context) {
@@ -35,18 +30,49 @@ class _MainAppState extends State<MainApp> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               ElevatedButton(
-                onPressed: () {
-                  getIPAddress();
-                  print(textData);
+                onPressed: () async {
+                  var _ = await getIPAddress();
+                  setState(() {
+                    localIp = _!;
+                  });
                 },
                 child: const Text('Fetch Data'),
               ),
               const SizedBox(height: 20.0),
-              Text(textData),
+              Text(localIp),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() async {
+                    discoveredDevices = (await getLocalDevicesIp());
+                    print(discoveredDevices);
+                  });
+                },
+                child: const Text('Get other device'),
+              ),
+              Container(
+                height: 200,
+                child: ListView.builder(
+                  itemCount: 5,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(title: Text(discoveredDevices[index]));
+                  },
+                ),
+              )
             ],
           ),
         ),
       ),
     );
   }
+}
+
+Future<String?> getIPAddress() async {
+  return await NetworkInfo().getWifiIP();
+}
+
+Future<List> getLocalDevicesIp() async {
+  String? wifiIP = await NetworkInfo().getWifiIP();
+  String subnet = ipToCSubnet(wifiIP!);
+  final List<Host> hosts = await LanScanner().quickIcmpScanAsync(subnet);
+  return hosts;
 }
